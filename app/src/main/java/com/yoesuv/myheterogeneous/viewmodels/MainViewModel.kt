6ce55e9ad7f2b8a4d8657209213a7762e7036530
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-09-15 09:13:14 +0700

    :pencil: setup viewmodel on main activity

---
+++ app/src/main/java/com/yoesuv/myheterogeneous/viewmodels/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.yoesuv.myheterogeneous.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+}
--- app/src/main/java/com/yoesuv/myheterogeneous/views/MainActivity.kt
+++ app/src/main/java/com/yoesuv/myheterogeneous/views/MainActivity.kt
@@ -2,12 +2,21 @@ package com.yoesuv.myheterogeneous.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.yoesuv.myheterogeneous.R
+import com.yoesuv.myheterogeneous.databinding.ActivityMainBinding
+import com.yoesuv.myheterogeneous.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.main = viewModel
     }
 }
